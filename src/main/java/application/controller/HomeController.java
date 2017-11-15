154e60f1c963c9f34615e339b3d1325c6447bb1d
MateuszGładysz <mateuszg1305@gmial.com>
2017-11-15 15:58:23 +0100

    First commit

---
+++ src/main/java/application/controller/HomeController.java
@@ -0,0 +1,25 @@
+package application.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(method= RequestMethod.GET)
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+    public String login() {
+        return "login";
+    }
+
+}
