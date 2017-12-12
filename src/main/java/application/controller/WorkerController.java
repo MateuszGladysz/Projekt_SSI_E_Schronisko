d2c90be5d60d87cb5861caf791209b6280fe339c
MateuszGładysz <mateuszg1305@gmial.com>
2017-12-12 09:34:27 +0100

    Done is for test

--- src/main/java/application/controller/WorkerController.java
+++ src/main/java/application/controller/WorkerController.java
@@ -1,10 +1,14 @@
 package application.controller;
 
 import application.model.Animal;
-import application.model.UserAccount;
 import application.service.AnimalService;
 import application.service.UserAccountService;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,55 +16,76 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.Map;
-
-@Slf4j
 @Controller
-@RequestMapping("/worker")
+@RequestMapping({"/worker"})
 public class WorkerController {
+    private static final Logger log = LoggerFactory.getLogger(WorkerController.class);
     @Autowired
     private AnimalService animalService;
-
+    @Autowired
+    private UserAccountService userAccountService;
     @Autowired
     private HttpSession session;
 
-    @RequestMapping(value = "/logout", method = {RequestMethod.GET})
-    public String logout() {
+    public WorkerController() {
+    }
 
-        session.invalidate();
+    @RequestMapping(
+            value = {"/logout"},
+            method = {RequestMethod.GET}
+    )
+    public String logout() {
+        this.session.invalidate();
         return "redirect:/";
     }
 
-    @RequestMapping(value = "/addAnimal", method = {RequestMethod.GET})
-    public String animal() {return "addAnimal"; }
+    @RequestMapping(
+            value = {"/addAnimal"},
+            method = {RequestMethod.GET}
+    )
+    public String animal() {
+        return "addAnimal";
+    }
 
+    @RequestMapping(
+            value = {"/addMoney"},
+            method = {RequestMethod.GET}
+    )
+    public String money() {
+        return "addMoney";
+    }
 
+    @RequestMapping(
+            value = {"/addMoney"},
+            method = {RequestMethod.POST}
+    )
+    public String submit(HttpServletRequest request, Map<String, Object> model) {
+        String message = this.userAccountService.updateBalance(request.getParameter("userEmail"), Integer.parseInt(request.getParameter("balance")));
+        if (message.equals("Balance add correctly")) {
+            model.put("balanceUpdateMessage", "Konto zostalo zaktualizowane");
+        } else {
+            model.put("balanceUpdateMessage", "Konto nie zostalo zaktualizowane");
+        }
 
-    @RequestMapping(value = "/addMoney", method = {RequestMethod.GET})
-    public String money() {return "addMoney"; }
+        return "addMoney";
+    }
 
-
-    @RequestMapping(value = "/addAnimal", method = {RequestMethod.POST})
-    public String addAnimal(Animal animal,@RequestParam("file")MultipartFile photo, Map<String,Object> model){
-
-
-
-        if(!photo.getOriginalFilename().contains(".jpg")){
-            model.put("addingFailureMessage","Proszę podać plik z roszerzeniem jpg");
+    @RequestMapping(
+            value = {"/addAnimal"},
+            method = {RequestMethod.POST}
+    )
+    public String addAnimal(Animal animal, @RequestParam("file") MultipartFile photo, Map<String, Object> model) {
+        if (!photo.getOriginalFilename().contains(".jpg")) {
+            model.put("addingFailureMessage", "Proszę podać plik z roszerzeniem jpg");
             return "/addAnimal";
+        } else {
+            try {
+                this.animalService.addAnimal(animal, photo);
+                return "redirect:/worker";
+            } catch (IOException var5) {
+                model.put("addingFailureMessage", "Problem z zapisem do bazy");
+                return "/addAnimal";
+            }
         }
-
-        try {
-            animalService.addAnimal(animal, photo);
-        } catch (IOException e) {
-            model.put("addingFailureMessage","Problem z zapisem do bazy");
-           return "/addAnimal";
-        }
-
-
-        return "redirect:/worker";
-
     }
 }
--- src/main/java/application/service/AnimalService.java
+++ src/main/java/application/service/AnimalService.java
@@ -2,38 +2,30 @@ package application.service;
 
 import application.model.Animal;
 import application.repository.AnimalRepository;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
 @Service
 public class AnimalService {
-
     @Autowired
     AnimalRepository animalRepository;
 
-    public void addAnimal(Animal animal, MultipartFile photo) throws IOException{
+    public AnimalService() {
+    }
 
+    public void addAnimal(Animal animal, MultipartFile photo) throws IOException {
         animal.setDonation(0);
         animal.setPhoto(animal.getName() + ".jpg");
-
-        Animal savedAnimal= animalRepository.save(animal);
-
-        String destination = "D:\\projekt_ssi\\Projekt_SSI_E_Schronisko\\src\\main\\resources\\public\\images\\"+savedAnimal.getName()+".jpg";
-
+        Animal savedAnimal = (Animal)this.animalRepository.save(animal);
+        String destination = "D:\\projekt_ssi\\Projekt_SSI_E_Schronisko\\src\\main\\resources\\public\\images\\" + savedAnimal.getName() + ".jpg";
         photo.transferTo(new File(destination));
-
     }
 
-    public List<Animal> getAllAnimals(){
-
-        return (List)animalRepository.findAll();
+    public List<Animal> getAllAnimals() {
+        return (List)this.animalRepository.findAll();
     }
-
-
-
 }
